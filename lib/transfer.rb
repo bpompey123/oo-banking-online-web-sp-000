b5517ca7fd66bb95ec2b50628769d0751adcb203
Brian Pompey <brianthescholar@gmail.com>
2019-10-06 03:14:46 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,13 @@ class Transfer
   end
 
   def execute_transaction
-
+    if valid? && sender.balance > amount && self.status == "pending"
+      sender.balance -= amount
+      receiver.balance += amount
+      @status = "complete"
+    else
+      reject_transfer
+    end
   end
 
 end
